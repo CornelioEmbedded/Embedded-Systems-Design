62d1547487ba7d67eede83e6543c1cde16c7a623
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-12 22:43:59 -0500

    delete comments from generate number

--- Act_2/Random_generator/Core/Src/main.c
+++ Act_2/Random_generator/Core/Src/main.c
@@ -1,46 +1,6 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
 int D1 = 0x70;
 int D2 = 0x60;
 int D3 = 0x50;
@@ -110,7 +70,7 @@ void setDisplay(int dig1, int dig2, int dig3, int dig4, int dig5,int dig6,int di
     HAL_Delay(1);
     GPIOC->ODR=D2+numeros[dig2];
     HAL_Delay(1);
-    GPIOC->ODR=D1+numeros[dig1]; //Decenas de Millones
+    GPIOC->ODR=D1+numeros[dig1];
     HAL_Delay(1);
 
 }
