bf601c4d104355c13e1578798b38591074855928
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-25 19:37:03 -0500

    trash code

--- Clock/Clock/Core/Src/main.c
+++ Clock/Clock/Core/Src/main.c
@@ -1,195 +1,216 @@
-
+/* USER CODE BEGIN Header */
+/**
+  **************************
+  * @file           : main.c
+  * @brief          : Main program body
+  **************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  **************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+//Variables
 ADC_HandleTypeDef hadc1;
+RTC_HandleTypeDef hrtc;
+int D1=0x70;//Y7
+int D2=0x60;
+int D3=0x50;
+int D4=0x40;
+int D5=0x30;
+int D6=0x20;
+int D7=0x10;
+int D8=0x00; //Y0
 
-int D1 = 0x70;
-int D2 = 0x60;
-int D3 = 0x50;
-int D4 = 0x40;
-int D5 = 0x30;
-int D6 = 0x20;
-int D7 = 0x10;
-int D8 = 0x00;
+int numero=0;
+int numeros[10]={0x0,0x01,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9};
+int u,count;
 
+uint16_t adcValue; //Asignacion de variable para el ADC
+int sec,dsec,min,dmin,hou,dhou,hora,horas,minutos,segundos,boton;
+
+int i=0x0;
+//Voids
+void setDisplay(int hora);
+void displayNumber(int numero);
+void cambioHora(int boton);
+
+
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
-void displayNumber(int valor);
-void setDisplay(int dig1, int dig2, int dig3, int dig4, int dig5, int dig6, int dig7, int dig8);
-void setDisplay_reloj(int dig1, int dig2, int dig4, int dig5, int dig7, int dig8);
-void displayReloj(int segundos, int minutos, int horas);
+static void MX_RTC_Init(void);
+/* USER CODE BEGIN PFP */
 
-uint32_t ADCValue = 0;
-uint32_t VP_segundos = 0;
-uint32_t VP_minutos = 0;
-uint32_t VP_horas = 0;
+/* USER CODE END PFP */
 
-uint32_t segundos_antes = 0;
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
-uint32_t segundos = 0;
-uint32_t minutos = 0;
-uint32_t horas = 0;
+/* USER CODE END 0 */
 
-int contador = 0;
-int numero = 0;
-
-int numeros[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
+  /* USER CODE BEGIN 1 */
 
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
 
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
+  MX_RTC_Init();
+  /* USER CODE BEGIN 2 */
+  RTC_TimeTypeDef sTime;
+    RTC_TimeTypeDef sDate;
+  /* USER CODE END 2 */
 
-  while (1)
-  {
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+    while (1)
+      {
+        /* USER CODE END WHILE */
+    	  displayNumber(numero);
+    	  if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0))
+    	  	  	{
+    	  	  	count=1;
+    	  	  	HAL_Delay(1000);
+    	  	  	cambioHora(boton);
+    	  	  	sTime.Hours=horas;
+    	  	  	sTime.Minutes=minutos;
+    	  	  	sTime.Seconds=segundos;
+    	  	  	HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+    	  	  	HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BCD);}
+        /* USER CODE BEGIN 3 */
+      }
+      /* USER CODE END 3 */
+    }
 
-	  HAL_ADC_Start(&hadc1);
-	    if(HAL_ADC_PollForConversion(&hadc1, 5)==HAL_OK)
-	    {
-	 	   ADCValue=HAL_ADC_GetValue(&hadc1);
-	    }
-	    HAL_ADC_Stop(&hadc1);
+    void cambioHora(int boton)
+    {
+    	while(count==1)
+    	{
+    	HAL_ADC_Start(&hadc1);
+        adcValue=(HAL_ADC_GetValue(&hadc1)/171);
+    	horas=adcValue;
+    	dhou=horas/10;
+    	hou=horas%10;
+    	setDisplay (hora);
+    	if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0)){
+    	count=2;
+    	HAL_Delay(1000);}
+    	}
 
-	    VP_segundos = (ADCValue*59)/4095;
-	    VP_minutos = (ADCValue*59)/4095;
-	    VP_horas = (ADCValue*23)/4095;
+    	while(count==2)
+    	{
+    	HAL_ADC_Start(&hadc1);
+    	adcValue=(HAL_ADC_GetValue(&hadc1)/68);
+    	minutos=adcValue;
+    	dmin=minutos/10;
+    	min=minutos%10;
+    	setDisplay (hora);
+    	if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0)){
+    	count=3;
+    	HAL_Delay(1000);}
+    	}
 
-	    if(contador == 0)
-	    {
-	    	displayNumber(0);
-	    }
+    	while(count==3)
+    	{
+    	HAL_ADC_Start(&hadc1);
+    	adcValue=(HAL_ADC_GetValue(&hadc1)/68);
+    	segundos=adcValue;
+    	dsec=segundos/10;
+    	sec=segundos%10;
+    	setDisplay (hora);
+    	if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0)){
+    	count=0;
+    	HAL_Delay(1000);}
+    	}
+    }
 
-	    else if(contador == 1)
-		{
-			segundos = VP_segundos;
-			displayNumber(segundos);
-		}else if(contador == 2)
-		{
-			minutos = VP_minutos;
-			displayNumber(minutos);
-		}else if(contador == 3)
-		{
-			horas = VP_horas;
-			displayNumber(horas);
-		}else if(contador == 4)
-		{
-			displayReloj(segundos, minutos, horas);
+    void displayNumber(int numero)
+    {
+    	RTC_TimeTypeDef sTime;
+    	RTC_TimeTypeDef sDate;
+    	HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BCD);
+    	HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BCD);
+    	dhou=(sTime.Hours/16);
+    	hou=(sTime.Hours%16);
+    	dmin=(sTime.Minutes/16);
+    	min=(sTime.Minutes%16);
+    	dsec=(sTime.Seconds/16);
+    	sec=(sTime.Seconds%16);
+    	setDisplay (hora);
+    }
 
-			if(segundos_antes <= 59)
-			{
-				segundos_antes++;
-			}else
-			{
-				if(segundos <= 59)
-				{
-					segundos_antes = 0;
-					segundos++;
-				}else
-				{
-					if(minutos <= 59)
-					{
-						segundos = 0;
-						minutos++;
-					}else
-					{
-						if(horas <= 23)
-						{
-							minutos = 0;
-							horas++;
-						}else
-						{
-							segundos = 0;
-							minutos = 0;
-							horas = 0;
-						}
-					}
-				}
-			}
-		}
+    void setDisplay(int hora)
+    {
 
-  }
-
-}
-
-
-
-void displayReloj(int segundos, int minutos, int horas)
-{
-	int dig1,dig2,dig4,dig5,dig7,dig8;
-
-	    dig8=segundos%10;
-	    dig7=(segundos%100)/10;
-	    dig5=(minutos%10);
-	    dig4=(minutos%100)/10;
-	    dig2=(horas%10);
-	    dig1=(horas%100)/10;
-
-	    setDisplay_reloj(dig1, dig2, dig4, dig5, dig7, dig8);
-
-}
-
-void displayNumber(int valor)
-  {
-    int dig1,dig2,dig3,dig4,dig5,dig6,dig7,dig8;
-
-    dig8=valor%10; //Unidades (8)
-    dig7=(valor%100)/10; //Decenas (7)
-    dig6=(valor%1000)/100; //Centenas (6)
-    dig5=(valor%10000)/1000; //millares almacenar el (5)
-    dig4=(valor%100000)/10000;//Decenas de millares (4)
-    dig3=(valor%1000000)/100000;//Centenas de millares (3)
-    dig2=(valor%10000000)/1000000; //Millones        (2)
-    dig1=(valor%100000000)/10000000;//Decenas de millones (1)
-
-    setDisplay(dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8);
-  }
-
-void setDisplay(int dig1, int dig2, int dig3, int dig4, int dig5,int dig6,int dig7,int dig8)
-  {
-
-    GPIOC->ODR=D8+numeros[dig8];// Unidades
-    HAL_Delay(1);
-    GPIOC->ODR=D7+numeros[dig7];// Decenas
-    HAL_Delay(1);
-    GPIOC->ODR=D6+numeros[dig6];
-    HAL_Delay(1);
-    GPIOC->ODR=D5+numeros[dig5];
-    HAL_Delay(1);
-    GPIOC->ODR=D4+numeros[dig4];
-    HAL_Delay(1);
-    GPIOC->ODR=D3+numeros[dig3];
-    HAL_Delay(1);
-    GPIOC->ODR=D2+numeros[dig2];
-    HAL_Delay(1);
-    GPIOC->ODR=D1+numeros[dig1];
-    HAL_Delay(1);
-
-}
-
-void setDisplay_reloj(int dig1, int dig2, int dig4, int dig5,int dig7,int dig8)
-  {
-
-    GPIOC->ODR=D8+numeros[dig8];// Unidades
-    HAL_Delay(1);
-    GPIOC->ODR=D7+numeros[dig7];// Decenas
-    HAL_Delay(1);
-    GPIOC->ODR=D5+numeros[dig5];
-    HAL_Delay(1);
-    GPIOC->ODR=D4+numeros[dig4];
-    HAL_Delay(1);
-    GPIOC->ODR=D2+numeros[dig2];
-    HAL_Delay(1);
-    GPIOC->ODR=D1+numeros[dig1];
-    HAL_Delay(1);
-
-}
-
+    	GPIOD->ODR=numeros[sec]+D8;
+    	HAL_Delay(1);
+    	GPIOD->ODR=numeros[dsec]+D7;
+    	HAL_Delay(1);
+    	//GPIOD->ODR=numeros[dig3]+D3;
+        //HAL_Delay(1);
+    	GPIOD->ODR=numeros[min]+D5;
+    	HAL_Delay(1);
+    	GPIOD->ODR=numeros[dmin]+D4;
+    	HAL_Delay(1);
+    	//GPIOD->ODR=numeros[dig6]+D6;
+    	//HAL_Delay(1);
+    	GPIOD->ODR=numeros[hou]+D2;
+    	HAL_Delay(1);
+    	GPIOD->ODR=numeros[dhou]+D1;
+    	HAL_Delay(1);
+    }
 
 void SystemClock_Config(void)
 {
@@ -229,58 +250,6 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ADC1_Init(void)
-{
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_0;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
-}
-
-/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -295,32 +264,26 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PC0 PC1 PC2 PC3
-                           PC4 PC5 PC6 PC7 */
+                           PC4 PC5 PC6 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PC8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
 }
 
@@ -336,10 +299,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -355,7 +315,7 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
